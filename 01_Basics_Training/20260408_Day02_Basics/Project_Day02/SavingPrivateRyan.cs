4fedb7b96959262837ea8d79ab5236ae38edaf2a
Avery-Sun <averysun.09@gmail.com>
2026-04-08 22:40:58 +0800

    Renew Folder2

---
+++ 01_Basics_Training/20260408_Day02_Basics/Project_Day02/SavingPrivateRyan.cs
@@ -0,0 +1,23 @@
+﻿namespace Project_Day02;
+
+public class SavingPrivateRyan : Film
+{
+    public string Actor = "Tom Hanks";
+    public SavingPrivateRyan() {
+
+        Name = "Saving Private Ryan";
+
+        Year = 1998;
+
+        Place = "USA";
+
+    }
+
+    
+
+
+
+}
+
+
+
